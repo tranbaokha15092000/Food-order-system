33356f016a99a9293c4dc1cb7fb7eceff015b0d1
Le Anh Hao <hao.le@keyreply.com>
2022-01-18 00:27:42 +0700

    add tailwindcss and create ui for menu creator

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <div className="w-full p-4 text-center font-semibold bg-slate-900 text-pink-600">
+            Powered by Dev Gang.
+        </div>
+    );
+}
+
+export default Footer;
